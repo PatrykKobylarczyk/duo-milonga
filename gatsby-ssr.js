4ff68477813938f2163591da596e8236fd235ed2
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2022-12-11 21:23:58 +0100

    add gatsby-ssr file

---
+++ gatsby-ssr.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./src/styles/global.css";
+
+import { RecoilRoot } from "recoil";
+import Layout from './src/components/Layout';
+
+export const wrapRootElement = ({element, data}) => <RecoilRoot {...data}>{element}</RecoilRoot>;
+export const wrapPageElement = ({element, data}) => <Layout {...data} >{element} </Layout>
