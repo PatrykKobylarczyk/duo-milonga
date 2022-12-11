5a590c024614675bf17ff4e35eb88d7cefd48999
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2022-12-11 21:37:56 +0100

    work with Static image

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -20,12 +20,11 @@ const About = () => {
           src="../assets/images/Duo/08.jpg"
           alt="main room"
           className=" absolute w-full h-screen z-0"
-          // objectPosition={`${isAboveMediumScreens ? "0% 0%" : "31% 0%"}`}
-          // objectFit={{}}
+          objectPosition={`${isAboveMediumScreens ? "0% 0%" : "31% 0%"}`}
         />
         <div className=" absolute left-0 top-0 w-full h-screen bg-gradient-layout"></div>
       </div>
-      <div className="page flex min-h-screen justify-center px-6 lg:justify-start lg:pl-40">
+      <div className="page flex justify-center px-6 lg:justify-start lg:pl-40">
         <div className="max-w-6xl h-full py-20 lg:py-32 px-10 w-3xl md:max-w-4xl ">
           <h1 className="text-3xl font-bold mb-12">{lang.about_title}</h1>
           <p>{lang.about_description_paragraph1}</p>
