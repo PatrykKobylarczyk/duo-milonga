56fb728ae710e5585eeb8687b0912332dfb97e9a
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2022-12-11 13:18:57 +0100

    try to repair deploy

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./src/styles/global.css";
 
 import { RecoilRoot } from "recoil";
-import Layout from './src/components/layout';
+import Layout from './src/components/Layout';
 
 export const wrapRootElement = ({element, data}) => <RecoilRoot {...data}>{element}</RecoilRoot>;
 export const wrapPageElement = ({element, data}) => <Layout {...data} >{element} </Layout>
