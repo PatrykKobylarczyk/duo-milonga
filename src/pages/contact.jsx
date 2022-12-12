1e6612cae9305847c30cd189fed44d69dc9daa6b
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2022-12-12 12:27:21 +0100

    repair Icon contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -8,6 +8,7 @@ import { useRecoilState } from "recoil";
 import { languageState } from "../atoms/atom";
 import { SlArrowRight } from "react-icons/sl";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Contact = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
@@ -31,31 +32,42 @@ const Contact = () => {
       </div>
       <div className="absolute left-0 top-0 w-full h-full bg-darker-gradient-bg z-10"></div>
       <div className="absolute top-0 left-0 w-full h-full flex flex-col lg:flex-row justify-center items-center px-10 sm:px-24 lg:px-40">
-        <div className="flex flex-col justify-start w-full z-20 overflow-x-auto gap-2">
-          <h3 className="text-xl lg:text-2xl font-light">
-            {lang.contact_title}
-          </h3>
-          <p className="text-2xl lg:text-3xl font-bold mt-5">
-            {lang.contact_name}
-          </p>
-          <ul className="text-lg mt-5 gap-4">
-            <li>
-              <a href="tel:48508239654">{lang.contact_phone}</a>
-            </li>
-            <li>
-              <a href="mailto:duomilonga@gmail.com">{lang.contact_mail}</a>
-            </li>
-            <li>
-              <a href="https://wa.me/48508239654">{lang.contact_whatsapp}</a>
-            </li>
-            <li
-              className="mt-5 flex items-center"
-              onClick={() => setIsContactForm((prev) => !prev)}
-            >
-              <button>{lang.contact_contact_form}</button>
-              <SlArrowRight size={"12px"} className="ml-4 mt-1" />
-            </li>
-          </ul>
+        <div className="flex flex-col justify-start w-full z-20">
+          {!isContactForm && (
+            <div className="flex flex-col   gap-2">
+              <h3 className="text-xl lg:text-2xl font-light">
+                {lang.contact_title}
+              </h3>
+              <p className="text-2xl lg:text-3xl font-bold mt-5">
+                {lang.contact_name}
+              </p>
+              <ul className="text-lg mt-5 gap-4">
+                <li>
+                  <a href="tel:48508239654">{lang.contact_phone}</a>
+                </li>
+                <li>
+                  <a href="mailto:duomilonga@gmail.com">{lang.contact_mail}</a>
+                </li>
+                <li>
+                  <a href="https://wa.me/48508239654">
+                    {lang.contact_whatsapp}
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+          <button
+            className="mt-5 flex items-center"
+            onClick={() => setIsContactForm((prev) => !prev)}
+          >
+            <button>{lang.contact_contact_form}</button>
+            <span className="ml-4 mt-1 rotate-90">
+              <SlArrowRight
+                size={"12px"}
+                className={`${isContactForm ? "rotate-180" : null}`}
+              />
+            </span>
+          </button>
         </div>
         {isContactForm && <ContactForm language={language} />}
       </div>
