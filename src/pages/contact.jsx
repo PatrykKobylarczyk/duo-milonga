dd631528a102cdb75c8ba05e6948dcd972e190b7
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2022-12-12 12:04:42 +0100

    work with RWD - contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -9,7 +9,6 @@ import { languageState } from "../atoms/atom";
 import { SlArrowRight } from "react-icons/sl";
 import { useState } from "react";
 
-
 const Contact = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   const [language, setLanguage] = useRecoilState(languageState);
@@ -18,48 +17,49 @@ const Contact = () => {
   const [isFormOpen, setFormOpen] = useState(false);
 
   return (
-      <div className="page pages relative h-screen flex flex-col justify-center overflow-hidden">
-        <div className="flex">
-          {isAboveMediumScreens && (
-            <div className="h-full w-1/6 bg-darker-gradient-bg"></div>
-          )}
-          <StaticImage
-            src="../assets/images/Duo/05.jpg"
-            alt="main room"
-            className="h-screen left-0 z-0"
-            objectPosition="65% 0"
-          />
-        </div>
-        <div className="absolute left-0 top-0 w-full h-full bg-darker-gradient-bg z-10"></div>
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col lg:flex-row justify-center items-center px-16 lg:px-40">
-          <div className="flex flex-col justify-start w-full z-20 overflow-x-auto gap-2">
-            <h3 className="text-xl lg:text-2xl font-light">
-              {lang.contact_title}
-            </h3>
-            <p className="text-2xl lg:text-3xl font-bold mt-5">
-              {lang.contact_name}
-            </p>
-            <ul className="text-lg mt-5 gap-4">
-              <li>
-                <a href="tel:48508239654">{lang.contact_phone}</a>
-              </li>
-              <li>
-                <a href="mailto:duomilonga@gmail.com">{lang.contact_mail}</a>
-              </li>
-              <li>
-                <a href="https://wa.me/48508239654">{lang.contact_whatsapp}</a>
-              </li>
-              <li className="mt-5 flex items-center">
-                <button onClick={() => setIsContactForm((prev) => !prev)}>
-                  {lang.contact_contact_form}
-                </button>
-                <SlArrowRight size={"12px"} className="ml-4 mt-1" />
-              </li>
-            </ul>
-          </div>
-          {isContactForm && <ContactForm language={language} />}
-        </div>
+    <div className="page pages relative h-screen flex flex-col justify-center overflow-hidden">
+      <div className="flex">
+        {isAboveMediumScreens && (
+          <div className="h-full w-1/6 bg-darker-gradient-bg"></div>
+        )}
+        <StaticImage
+          src="../assets/images/Duo/05.jpg"
+          alt="main room"
+          className="h-screen left-0 z-0"
+          objectPosition="62% 0"
+        />
       </div>
+      <div className="absolute left-0 top-0 w-full h-full bg-darker-gradient-bg z-10"></div>
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col lg:flex-row justify-center items-center px-10 sm:px-24 lg:px-40">
+        <div className="flex flex-col justify-start w-full z-20 overflow-x-auto gap-2">
+          <h3 className="text-xl lg:text-2xl font-light">
+            {lang.contact_title}
+          </h3>
+          <p className="text-2xl lg:text-3xl font-bold mt-5">
+            {lang.contact_name}
+          </p>
+          <ul className="text-lg mt-5 gap-4">
+            <li>
+              <a href="tel:48508239654">{lang.contact_phone}</a>
+            </li>
+            <li>
+              <a href="mailto:duomilonga@gmail.com">{lang.contact_mail}</a>
+            </li>
+            <li>
+              <a href="https://wa.me/48508239654">{lang.contact_whatsapp}</a>
+            </li>
+            <li
+              className="mt-5 flex items-center"
+              onClick={() => setIsContactForm((prev) => !prev)}
+            >
+              <button>{lang.contact_contact_form}</button>
+              <SlArrowRight size={"12px"} className="ml-4 mt-1" />
+            </li>
+          </ul>
+        </div>
+        {isContactForm && <ContactForm language={language} />}
+      </div>
+    </div>
   );
 };
 
