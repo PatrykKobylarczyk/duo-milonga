051d90da2c1c91e0b318060a2ff67049f91b58c3
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2022-12-29 07:17:31 +0100

    translate music player for ios up and repair background img

--- src/pages/video.jsx
+++ src/pages/video.jsx
@@ -10,12 +10,12 @@ import { videoData } from "../data/videoData";
 const video = () => {
   return (
     <div className="relative w-full flex flex-col items-center pb-32">
-      <div className="fixed left-0 top-0 w-full h-full bg-gradient-layout-darker"></div>
+      <div className="fixed left-0 top-0 w-full h-screen bg-gradient-layout-darker"></div>
       <h1 className="my-40 text-3xl md:text-6xl md:translate-y-16 text-white z-[7]">Videos</h1>
       <StaticImage
         src="../assets/images/Duo/04.jpg"
         alt="main room"
-        className="h-full fixed left-0 top-0 -z-[2]"
+        className="h-screen fixed left-0 top-0 -z-[2]"
         objectPosition=" 65% 0"
       />
       <div className="w-full grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 z-30 px-5 lg:p-20">
