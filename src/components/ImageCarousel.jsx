69b8d56edb756819524ffd1dfa0aa15c9e1c9fe0
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-01-14 22:19:43 +0100

    work with dontrol btns in gallery

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -24,14 +24,16 @@ const ImageCarousel = ({
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full h-screen grid place-items-center bg-black/80 z-[500]">
-      <div className="relative sm:max-w-[90%] sm:h-[90vh] flex items-center gap-10">
-        <div className="absolute scale-75 sm:top-0 sm:right-0 z-[800] text-white text-sm opacity-40 hover:opacity-80 transition duration-200">
-          <Button
-            content={<MdClose />}
-            handleClick={() => setShowModal(false)}
-          />
-        </div>
+    <div className="fixed top-0 left-0 w-full h-screen grid place-items-center bg-black/90 z-[500]">
+      <div className="relative h-[80vh] sm:max-w-[90%] sm:h-[90vh] flex items-center gap-10">
+        {isAboveMediumScreens && (
+          <div className="absolute scale-75 top-0 right-0 z-[800] text-white text-sm opacity-40 hover:opacity-80 transition duration-200">
+            <Button
+              content={<MdClose />}
+              handleClick={() => setShowModal(false)}
+            />
+          </div>
+        )}
         {isAboveMediumScreens && (
           <div className="text-white scale-75 opacity-40 hover:opacity-80 transition duration-200">
             <Button
@@ -43,20 +45,20 @@ const ImageCarousel = ({
             />
           </div>
         )}
-        
-          <GatsbyImage
-            image={
-              data.allFile.edges[currentIndex].node.childImageSharp
-                .gatsbyImageData
-            }
-            alt="gallery"
-            className="w-auto max-h-full"
-            imgStyle={{
-              objectFit: "contain",
-              objectPosition: "50% 50%",
-            }}
-          />
-        
+
+        <GatsbyImage
+          image={
+            data.allFile.edges[currentIndex].node.childImageSharp
+              .gatsbyImageData
+          }
+          alt="gallery"
+          className="w-auto max-h-full"
+          imgStyle={{
+            objectFit: "contain",
+            objectPosition: "50% 50%",
+          }}
+        />
+
         {isAboveMediumScreens && (
           <div className="text-white scale-75 opacity-40 hover:opacity-80 transition duration-200">
             <Button
@@ -69,6 +71,34 @@ const ImageCarousel = ({
           </div>
         )}
       </div>
+      {!isAboveMediumScreens && (
+        <div className="absolute flex gap-2 bottom-2">
+          <div className="text-white scale-75 opacity-60 hover:opacity-90 transition duration-200">
+            <Button
+              content={<MdWest />}
+              styles={`text-sm font-light text-white ${
+                currentIndex === 0 && "opacity-0 pointer-events-none"
+              }`}
+              handleClick={() => handleGalleryNavigation("prev")}
+            />
+          </div>
+          <div className="scale-75 z-[800] text-white text-sm opacity-60 sm:opacity-40 hover:opacity-90 transition duration-200">
+            <Button
+              content={<MdClose />}
+              handleClick={() => setShowModal(false)}
+            />
+          </div>
+          <div className="text-white scale-75 opacity-60 hover:opacity-90 transition duration-200">
+            <Button
+              content={<MdEast />}
+              styles={`text-sm font-light ${
+                currentIndex === 24 && "opacity-0 pointer-events-none"
+              }`}
+              handleClick={() => handleGalleryNavigation("next")}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
