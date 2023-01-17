7f192f8ba217e54afbed7ace53e15576873b4616
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-01-17 21:42:32 +0100

    changes image carousel

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,77 +1,76 @@
-import React from "react";
-import { GatsbyImage } from "gatsby-plugin-image";
-import Button from "../components/Button";
+import React, { useRef } from "react";
 import { MdWest, MdEast, MdClose } from "react-icons/md";
+import { GatsbyImage } from "gatsby-plugin-image";
+import Button from "./Button";
+
+//CAROUSEL
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 // HOOKS
 import useMediaQuery from "../hooks/useMediaQuery";
 
-const ImageCarousel = ({
+const ImageCarousel2 = ({
   setShowModal,
   currentIndex,
   setCurrentIndex,
   data,
 }) => {
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const slider = React.useRef(null);
+  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
 
-  const handleGalleryNavigation = (direction) => {
-    if (direction === "prev") {
-      setCurrentIndex((prev) => prev - 1);
-    }
-    if (direction === "next") {
-      setCurrentIndex((prev) => prev + 1);
-    }
+  const displayCarouselSection = data.allFile.edges.map((image) => (
+    <div className="w-full h-screen flex justify-center align-center">
+      <GatsbyImage
+        key={image.id}
+        image={image.node.childImageSharp.gatsbyImageData}
+        alt={"Duo milonga"}
+        className="w-full h-full"
+        imgStyle={{
+          objectFit: "contain",
+          objectPosition: "50% 50%",
+        }}
+      />
+    </div>
+  ));
+
+  const settings = {
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className: "w-full sm:w-[90vw] h-full",
+    initialSlide: currentIndex,
+    arrows: false,
+    adaptiveHeight: true,
   };
 
   return (
-    <div className="pages fixed top-0 left-0 w-full h-screen grid place-items-center bg-black/95 z-[500]">
-      <div className="relative h-[80vh] sm:max-w-[90%] sm:h-[90vh] flex items-center gap-10">
-        {isAboveMediumScreens && (
-          <div className="absolute scale-75 top-0 right-0 z-[800] text-white text-sm opacity-40 hover:opacity-80 transition duration-200">
-            <Button
-              content={<MdClose />}
-              handleClick={() => setShowModal(false)}
-            />
-          </div>
-        )}
-        {isAboveMediumScreens && (
-          <div className="text-white scale-75 opacity-40 hover:opacity-80 transition duration-200">
-            <Button
-              content={<MdWest />}
-              styles={`text-sm font-light text-white ${
-                currentIndex === 0 && "opacity-0 pointer-events-none"
-              }`}
-              handleClick={() => handleGalleryNavigation("prev")}
-            />
-          </div>
-        )}
-
-        <GatsbyImage
-          image={
-            data.allFile.edges[currentIndex].node.childImageSharp
-              .gatsbyImageData
-          }
-          alt="gallery"
-          className="w-auto max-h-full"
-          imgStyle={{
-            objectFit: "contain",
-            objectPosition: "50% 50%",
-          }}
+    <div className="pages fixed top-0 left-0 w-full h-screen flex items-center justify-center ali gap-10 bg-black/95 z-[500]">
+      {isAboveSmallScreens && (
+        <div className="absolute scale-75 top-0 right-0 z-[800] text-white text-sm opacity-40 hover:opacity-80 transition duration-200">
+          <Button
+            content={<MdClose />}
+            handleClick={() => setShowModal(false)}
+          />
+        </div>
+      )}
+      {isAboveSmallScreens && (
+        <Button
+          content={<MdWest />}
+          handleClick={() => slider?.current?.slickPrev()}
         />
-
-        {isAboveMediumScreens && (
-          <div className="text-white scale-75 opacity-40 hover:opacity-80 transition duration-200">
-            <Button
-              content={<MdEast />}
-              styles={`text-sm font-light ${
-                currentIndex === 24 && "opacity-0 pointer-events-none"
-              }`}
-              handleClick={() => handleGalleryNavigation("next")}
-            />
-          </div>
-        )}
-      </div>
-      {!isAboveMediumScreens && (
+      )}
+      <Slider ref={slider} {...settings}>
+        {displayCarouselSection}
+      </Slider>
+      {isAboveSmallScreens && (
+        <Button
+          content={<MdEast />}
+          handleClick={() => slider?.current?.slickNext()}
+        />
+      )}
+      {!isAboveSmallScreens && (
         <div className="absolute flex gap-2 bottom-2">
           <div className="text-white scale-75 opacity-60 hover:opacity-90 transition duration-200">
             <Button
@@ -79,12 +78,12 @@ const ImageCarousel = ({
               styles={`text-lg font-light text-white ${
                 currentIndex === 0 && "opacity-0 pointer-events-none"
               }`}
-              handleClick={() => handleGalleryNavigation("prev")}
+              handleClick={() => slider?.current?.slickPrev()}
             />
           </div>
           <div className="scale-75 z-[800] text-white text-sm opacity-60 sm:opacity-40 hover:opacity-90 transition duration-200">
             <Button
-              content={<MdClose size={'17px'}/>}
+              content={<MdClose size={"17px"} />}
               handleClick={() => setShowModal(false)}
             />
           </div>
@@ -94,7 +93,7 @@ const ImageCarousel = ({
               styles={`text-lg font-light ${
                 currentIndex === 24 && "opacity-0 pointer-events-none"
               }`}
-              handleClick={() => handleGalleryNavigation("next")}
+              handleClick={() => slider?.current?.slickNext()}
             />
           </div>
         </div>
@@ -103,4 +102,4 @@ const ImageCarousel = ({
   );
 };
 
-export default ImageCarousel;
+export default ImageCarousel2;
