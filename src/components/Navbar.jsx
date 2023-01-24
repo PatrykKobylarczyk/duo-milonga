b16ae6c465ba3feba63fff1456c1b19691ada687
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-01-24 11:34:09 +0100

    work with animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -113,7 +113,7 @@ const Navbar = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ ease: "easeInOut", duration: 0.15 }}
+            transition={{ ease: "easeInOut", duration: 0.3 }}
           >
             {/* MENU ITEMS */}
             <AnimatePresence>
